f8921d63ce668c176143f8bed0bf650cfbc86037
Gerardo Delgado <motcrash@gmail.com>
2025-06-25 16:13:00 -0600

    Fix component import and routing for OrdersHeaderComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RolesGridComponent } from './roles-table/roles-grid.component';
-import { DataGridComponent } from './ordersHeader-grid/ordersHeader-grid.component';
+import { OrdersHeaderComponent } from './ordersHeader-grid/ordersHeader-grid.component';
 import { UsersTableComponent } from './users-table/users-table.component';
 import { RoleViewsComponent } from './roleViews/roleViews-table.component';
 
 const routes: Routes = [
-  { path: 'orders-header', component: DataGridComponent},
+  { path: 'orders-header', component: OrdersHeaderComponent},
   { path: 'roles', component: RolesGridComponent},
   { path: 'users', component: UsersTableComponent},
   { path: 'role-views', component: RoleViewsComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DxBulletModule, DxDataGridModule, DxSelectBoxModule, DxTemplateModule } from 'devextreme-angular';
-import { DataGridComponent } from './ordersHeader-grid/ordersHeader-grid.component';
+import { OrdersHeaderComponent } from './ordersHeader-grid/ordersHeader-grid.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RolesGridComponent } from './roles-table/roles-grid.component';
 import { UsersTableComponent } from './users-table/users-table.component';
 import { RoleViewsComponent } from './roleViews/roleViews-table.component';
 
+import { DxBulletModule,
+  DxDataGridModule,
+  DxSelectBoxModule,
+  DxTemplateModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DataGridComponent,
+    OrdersHeaderComponent,
     SidebarComponent,
     RolesGridComponent,
     UsersTableComponent,
@@ -30,7 +33,7 @@ import { RoleViewsComponent } from './roleViews/roleViews-table.component';
     DxDataGridModule,
     DxTemplateModule,
     DxBulletModule,
-    DxSelectBoxModule
+    DxSelectBoxModule,
   ],
   providers: [],
   bootstrap: [AppComponent] 
