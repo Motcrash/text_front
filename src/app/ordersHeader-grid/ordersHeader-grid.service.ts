0d66f1b842942e66b80e5bcc75a57663e0d695c7
Gerardo Delgado <motcrash@gmail.com>
2025-06-26 15:49:23 -0600

    Refactor OrdersHeaderService: update API endpoints and add methods for order details handling

--- src/app/ordersHeader-grid/ordersHeader-grid.service.ts
+++ src/app/ordersHeader-grid/ordersHeader-grid.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, forkJoin, Observable, retry, tap, throwError } from 'rxjs';
 import 'devextreme/data/odata/store';
 import DataSource from 'devextreme/data/data_source';
 
 interface IOrdersHeader {
+  salesOrderId: number,
   revisionNumber: number,
   orderDate: Date,
   dueDate: Date,
@@ -28,18 +29,40 @@ interface IOrdersHeader {
   comment: string
 }
 
+interface IDetails {
+  salesOrderId: number,
+  salesOrderDetailId: number,
+  carrierTrackingNumber: string,
+  orderQty: number,
+  productId: number,
+  specialOfferId: number,
+  unitPrice: number,
+  unitPriceDiscount: number,
+  lineTotal: number,
+  rowguid: string,
+  modifiedDate: Date,
+  salesOrder: any[],
+  specialOfferProduct: any[]
+}
+
 @Injectable()
 export class OrdersHeaderService {
-  private apiUrl = 'https://localhost:7229/api/SalesOrderHeader';
+  private orderHeaderApi = 'https://localhost:7229/api/SalesOrderHeader';
+  private orderDetailsApi = 'https://localhost:7229/api/SalesOrderDetail';
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any[]> {
-    var res = this.http.get<IOrdersHeader[]>(this.apiUrl);
+    var res = this.http.get<IOrdersHeader[]>(this.orderHeaderApi);
+    
     return res;
   }
 
+  getLastDetail(): Observable<IOrdersHeader> {
+    return this.http.get<IOrdersHeader>(`${this.orderHeaderApi}/last`);
+  }
+
   create(order: IOrdersHeader): Observable<IOrdersHeader> {
-    return this.http.post<IOrdersHeader>(this.apiUrl, order).pipe(
+    return this.http.post<IOrdersHeader>(this.orderHeaderApi, order).pipe(
       tap(() => console.log('POST Success')),
       catchError((error) => {
         throw error;
@@ -48,7 +71,8 @@ export class OrdersHeaderService {
   }
 
   update(id: number, role: Partial<IOrdersHeader>): Observable<IOrdersHeader> {
-    return this.http.patch<IOrdersHeader>(`${this.apiUrl}/${id}`, role).pipe(
+    return this.http.patch<IOrdersHeader>(`${this.orderHeaderApi}/${id}`, role).pipe(
+      
       tap(() => console.log('Patch Success')),
       catchError((error) => {
         console.error('Patch Error');
@@ -58,7 +82,8 @@ export class OrdersHeaderService {
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+    return this.http.delete<void>(`${this.orderHeaderApi}/${id}`).pipe(
+      
       tap(() => console.log('Delete Success')),
       catchError((error: HttpErrorResponse): Observable<never> => {
         console.error('Delete Error');
@@ -79,4 +104,21 @@ export class OrdersHeaderService {
       pageSize: 20,
     });
   }
+
+  getOrderDetails(orderId: number): Observable<IDetails[]> {
+    return this.http.get<IDetails[]>(`${this.orderDetailsApi}/${orderId}`);
+  }
+
+  createOrderDetails(orderDetails: IDetails[]): Observable<IDetails[]> {
+  const requests = orderDetails.map(detail => {
+    return this.http.post<IDetails>(this.orderDetailsApi, detail).pipe(
+      tap(() => console.log('POST Success for detail:', detail)),
+      catchError((error) => {
+        console.error('POST Error for detail:', detail, error);
+        throw error;
+      })
+    );
+  });
+  return forkJoin(requests);
+}
 }
